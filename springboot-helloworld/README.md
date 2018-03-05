b6c5b2a84547eebab5c0bbf3098c0c936f539b56
zhanghaichang <zhanghaichang@163.com>
2018-03-05 16:09:01 +0800

    springboot hello world

---
+++ springboot-helloworld/README.md
@@ -0,0 +1,7 @@
+# Springboot + Hello World
+
+
+- 开发环境：Windows 10
+- ide：Eclipse Nemo
+- jdk：1.8
+- Spring-Boot：2.0.0.RELEASE
---
+++ springboot-helloworld/src/main/java/com/dwring/springboot/Application.java
@@ -0,0 +1,21 @@
+package com.dwring.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+ 
+/**  
+* @ClassName: Application  
+* @Description:  
+* @author haichangzhang  
+* @date 2017年7月26日 下午6:06:47  
+*    
+*/
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
