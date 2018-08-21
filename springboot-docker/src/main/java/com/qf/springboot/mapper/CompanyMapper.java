7fd1f086518be80ffce6457634e0c4045b09150a
Haichang Zhang <haichangzhang@quarkfinance.com>
2018-08-21 11:36:14 +0800

    springboot docker

--- springboot-docker/src/main/java/com/qf/springboot/mapper/CompanyMapper.java
+++
@@ -1,7 +0,0 @@
-package com.qf.springboot.mapper;
-
-import com.qf.springboot.model.Company;
-import com.qf.springboot.util.MyMapper;
-
-public interface CompanyMapper extends MyMapper<Company> {
-}
