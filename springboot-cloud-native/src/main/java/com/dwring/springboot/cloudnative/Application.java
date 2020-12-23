9affbab328343cf43f5f8906a873263b8f8edafe
zhanghaichang <zhanghaichang@163.com>
2020-12-23 16:54:01 +0800

    SprintBoot 2.4.1

---
+++ springboot-cloud-native/src/main/java/com/dwring/springboot/cloudnative/Application.java
@@ -0,0 +1,20 @@
+package com.dwring.springboot.cloudnative;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+* @ClassName: Application
+* @Description: SpringBoot 2.4.1
+* @author zhanghaichang
+* @date 2020年12月23日
+*
+*/
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
